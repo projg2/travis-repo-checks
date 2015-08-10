d400cb16e1c1c4613141ccb6400cf31186bb787b
Michał Górny <mgorny@gentoo.org>
2015-08-10 08:57:12 +0200

    Split run-tests into core & formatting parts

---
+++ run-pkgcheck.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e -x
+
+JOB=${1}
+NO_JOBS=${2}
+
+if [[ ! ${JOB} || ! ${NO_JOBS} ]]; then
+	# simple whole-repo run
+	exec pkgcheck -r gentoo --reporter FancyReporter \
+		-d imlate -d unstable_only -d cleanup -d stale_unstable \
+		--profile-disable-dev --profile-disable-exp
+elif [[ ${JOB} == global ]]; then
+	# global check part of split run
+	exec pkgcheck -r gentoo --reporter FancyReporter \
+		-c UnusedGlobalFlags -c UnusedLicense
+else
+	# keep the category scan silent, it's so loud...
+	set +x
+	cx=0
+	cats=()
+	for c in $( cd metadata/md5-cache; du $(<../../profiles/categories) | sort -n -r | cut -d$'\t' -f2)
+	do
+		if [[ $(( cx++ % ${NO_JOBS} )) -eq ${JOB} ]]; then
+			cats+=( "${c}/*" )
+		fi
+	done
+	set -x
+
+	exec pkgcheck -r gentoo --reporter FancyReporter "${cats[@]}" \
+		-d imlate -d unstable_only -d cleanup -d stale_unstable \
+		-d UnusedGlobalFlags -d UnusedLicense \
+		--profile-disable-dev --profile-disable-exp
+fi
--- run-tests.bash
+++ run-tests.bash
@@ -1,35 +1,6 @@
 #!/bin/bash
 set -e -x
 
-JOB=${1}
-NO_JOBS=${2}
-
-if [[ ! ${JOB} || ! ${NO_JOBS} ]]; then
-	# simple whole-repo run
-	pkgcheck -r gentoo --reporter FancyReporter \
-		-d imlate -d unstable_only -d cleanup -d stale_unstable \
-		--profile-disable-dev --profile-disable-exp
-elif [[ ${JOB} == global ]]; then
-	# global check part of split run
-	pkgcheck -r gentoo --reporter FancyReporter \
-		-c UnusedGlobalFlags -c UnusedLicense
-else
-	# keep the category scan silent, it's so loud...
-	set +x
-	cx=0
-	cats=()
-	for c in $( cd metadata/md5-cache; du $(<../../profiles/categories) | sort -n -r | cut -d$'\t' -f2)
-	do
-		if [[ $(( cx++ % ${NO_JOBS} )) -eq ${JOB} ]]; then
-			cats+=( "${c}/*" )
-		fi
-	done
-	set -x
-
-	pkgcheck -r gentoo --reporter FancyReporter "${cats[@]}" \
-		-d imlate -d unstable_only -d cleanup -d stale_unstable \
-		-d UnusedGlobalFlags -d UnusedLicense \
-		--profile-disable-dev --profile-disable-exp
-fi |& awk -f "$(dirname "${0}")"/parse-pcheck-output.awk
+bash ./run-pkgcheck.bash "${@}" |& awk -f "$(dirname "${0}")"/parse-pcheck-output.awk
 
 [[ ${PIPESTATUS[0]} == 0 ]]
